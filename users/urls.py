ed668a5e973cf3cf60bed47aba5c61bf294cd591
deeborges <deyvisonsilvaborges@gmail.com>
2019-05-16 16:36:54 -0300

    Base de dados sqlite3

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.urls import path
-from . import views
-
-# Configuração de rotas
-
-urlpatterns = [
-    path('', views.initalPage)
-]
--- users/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def initalPage(request):
-    return "Bem vindo a pagina de contas"
